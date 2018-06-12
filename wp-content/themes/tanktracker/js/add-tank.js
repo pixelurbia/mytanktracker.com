c1e83e693d8bdc29e693d94f21ae6d148f2caea8
pixelurbia <andrew@pixelurbia.com>
2018-06-11 19:55:54 -0500

    polyfill

--- wp-content/themes/tanktracker/js/add-tank.js
+++ wp-content/themes/tanktracker/js/add-tank.js
@@ -7,6 +7,18 @@
 $(document).ready(function() {
       
 
+//polyfill for IE support because it's shit and I hate it. 
+  if (!Object.entries)
+  Object.entries = function( obj ){
+    var ownProps = Object.keys( obj ),
+        i = ownProps.length,
+        resArray = new Array(i); // preallocate the Array
+    while (i--)
+      resArray[i] = [ownProps[i], obj[ownProps[i]]];
+
+    return resArray;
+  };
+
 //skip Tank add
   $('#skip-add-tank').click(function() { 
     tankForm();
