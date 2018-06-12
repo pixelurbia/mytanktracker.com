596acf133f7b37aebed1d2a61874067d91f90b2a
pixelurbia <andrew@pixelurbia.com>
2018-06-11 20:16:55 -0500

    polyfill

--- wp-content/themes/tanktracker/templates/header.php
+++ wp-content/themes/tanktracker/templates/header.php
@@ -13,7 +13,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
-
+<script src="//cdn.polyfill.io/v1/polyfill.min.js" async defer></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/polyfill.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/master.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/register-master.js"></script>
