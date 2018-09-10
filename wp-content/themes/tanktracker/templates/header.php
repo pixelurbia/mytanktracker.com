3b6cacaf4bd5ef9ffdf01ded2e13ef1b4e7f922a
pixelurbia <andrew@pixelurbia.com>
2018-09-09 20:29:22 -0500

    google analytics

--- wp-content/themes/tanktracker/templates/header.php
+++ wp-content/themes/tanktracker/templates/header.php
@@ -33,6 +33,18 @@
 
 	
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125182216-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-125182216-1');
+</script>
+
+
 	<?php wp_head(); ?>
 
 <!-- FAVICONS -->
