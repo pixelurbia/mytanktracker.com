ff4458055471b32768c608f548656c64424ada6c
pixelurbia <andrew@pixelurbia.com>
2018-09-11 18:33:58 -0500

    seo stuff

--- wp-content/themes/tanktracker/functions/general.php
+++ wp-content/themes/tanktracker/functions/general.php
@@ -14,3 +14,20 @@ function resizeImageFiles($size,$load,$target) {
     // return $target_file; //return name of saved file in case you want to store it in you database or show confirmation message to user   
 }
 }
+
+
+
+// define the custom replacement callback
+function get_myname() {
+		 	$current_user = wp_get_current_user();
+		$user = $current_user->ID;
+		return $user;
+}
+
+// define the action for register yoast_variable replacments
+function register_custom_yoast_variables() {
+    wpseo_register_var_replacement( '%%myname%%', 'get_myname', 'advanced', 'some help text' );
+}
+
+// Add action
+add_action('wpseo_register_extra_replacements', 'register_custom_yoast_variables');
